c40e3465d0e86de9a405a9eaaf5f2f64589d3b2d
Sukhendu Chakraborty <sukhenduchakraborty831@gmail.com>
2026-08-08 22:06:01 +0530

    Time: 0 ms (100%), Space: 19.5 MB (23.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        if n%2==1:
+            return False
+        st  = []
+        for ch in s:
+            if ch == '(' or ch == '{'or ch == '[':
+                st.append(ch)
+            else:
+                if len(st)==0:
+                    return False
+                top = st.pop()
+                if ch == ')' and top!='(':
+                    return False
+                elif ch == '}' and top!='{':
+                    return False
+                elif ch == ']' and top!='[':
+                    return False
+        return  len(st)==0
+            
+                
+
+
+
+
+
+
+        
