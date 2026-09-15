3650684193cf048904987c3436214c5cbd94e14d
Sukhendu Chakraborty <sukhenduchakraborty831@gmail.com>
2026-09-15 16:19:51 +0530

    Time: 0 ms (100%), Space: 19.1 MB (93.92%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        i,j = 0,0
+        n1, n2 = len(version1), len(version2)
+        while i<n1 or j<n2:
+            num1 = 0
+            num2 = 0
+            while i <n1 and version1[i]!=".":
+                num1 = num1*10 + int(version1[i])
+                i+=1
+            while j <n2 and version2[j]!=".":
+                num2 = num2*10 + int(version2[j])
+                j+=1
+            if num1>num2:
+                return 1
+            if num1<num2:
+                return -1
+            i+=1
+            j+=1
+        return 0
+            
